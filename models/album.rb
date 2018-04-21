dceb424ab425a3dfd0bf7b26c6d45630fae27294
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-21 09:10:09 +0100

    add wireframes to planning/

--- models/album.rb
+++ models/album.rb
@@ -3,9 +3,9 @@ class Album
   def initialize(options)
     @id = options['id'].to_i
     @title = options['title']
-    @stock = options['stock']
+    @stock = options['stock'].to_i
     @genre = options['genre']
-    @artist_id = options['artist_id']
+    @artist_id = options['artist_id'].to_i
   end
 
 end
