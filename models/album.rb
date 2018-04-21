e8564104b4c40aeff34452b707c59881aa689e80
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-21 11:01:39 +0100

    Artist and Album have delete method

--- models/album.rb
+++ models/album.rb
@@ -26,6 +26,12 @@ class Album
     SqlRunner.run(sql, values)
   end
 
+  def delete()
+    sql = "DELETE FROM albums WHERE id = $1;"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all()
     sql = "SELECT * FROM albums;"
     albums = SqlRunner.run(sql)
