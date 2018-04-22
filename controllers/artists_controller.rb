60a8ce8da288836cfa22babe7bb6158f18ff190e
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-22 21:16:01 +0100

    create view to add new Artist

--- controllers/artists_controller.rb
+++ controllers/artists_controller.rb
@@ -8,6 +8,16 @@ get '/artists' do
   erb(:"artists/index")
 end
 
+get '/artists/new' do
+  erb(:"artists/new")
+end
+
+post '/artists' do
+  artist = Artist.new(params)
+  artist.save()
+  redirect to '/artists'
+end
+
 get '/artists/:id' do
   @artist = Artist.find_by_id(params['id'].to_i)
   erb(:"artists/show")
