39e8375d5bf5a53ceb9c142c385a021e72643031
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-22 14:53:13 +0100

    add view to create new Album

--- controllers/albums_controller.rb
+++ controllers/albums_controller.rb
@@ -8,6 +8,18 @@ get '/albums' do
   erb(:"albums/index")
 end
 
+
+get '/albums/new' do
+  @artists = Artist.all()
+  erb(:"albums/new")
+end
+
+post '/albums' do
+  album = Album.new(params)
+  album.save()
+  redirect to "/albums"
+end
+
 get '/albums/:id' do
   @album = Album.find_by_id(params['id'].to_i)
   erb(:"albums/show")
