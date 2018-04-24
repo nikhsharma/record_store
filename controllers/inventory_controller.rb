f1b684837af3702a896bc19ef90317c1dec322a9
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-04-24 15:19:26 +0100

    style homepage tiles, can update stock from inventory page

--- controllers/inventory_controller.rb
+++ controllers/inventory_controller.rb
@@ -5,3 +5,10 @@ get '/inventory' do
   @artists = Artist.all()
   erb(:"/inventory/index")
 end
+
+post '/inventory/:id/update' do
+  @album = Album.find_by_id(params['id'])
+  @album.stock = params['stock']
+  @album.update()
+  redirect to '/inventory'
+end
